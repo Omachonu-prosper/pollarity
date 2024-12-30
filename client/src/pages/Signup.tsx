06806d1d40c2a613702282f5e90f6341c047b642
Omachonu Prosper <omachonucodes@gmail.com>
2024-12-30 21:53:40 +0100

    Create a user

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,5 +1,7 @@
-import InputField from "../components/InputField";
+import { useState } from "react";
+import { signup } from "../services/api";
 import { Navigate, Link } from "react-router";
+import InputField from "../components/InputField";
 
 interface Props {
   isAuthenticated: boolean;
@@ -8,17 +10,38 @@ interface Props {
 function Signup({ isAuthenticated }: Props) {
   if (isAuthenticated) return <Navigate to="/dashboard" />;
 
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  function onChangeListener(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setForm((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+
   return (
     <div className="container mx-auto px-4 mt-16 max-w-md">
       <h1 className="mb-5 text-2xl font-semibold text-gray-900">Signup</h1>
 
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          signup(form.username, form.email, form.password);
+        }}
+      >
         <InputField
           label="Username"
           type="text"
           name="username"
           id="username"
           placeholder="John Doe"
+          required={true}
+          onChange={onChangeListener}
         />
 
         <InputField
@@ -28,15 +51,19 @@ function Signup({ isAuthenticated }: Props) {
           id="email"
           placeholder="john@example.com"
           classNames="mt-3"
+          required={true}
+          onChange={onChangeListener}
         />
 
         <InputField
-          label="Passwod"
+          label="Password"
           type="password"
           name="password"
           id="password"
           placeholder="xxxxxx"
           classNames="mt-3"
+          required={true}
+          onChange={onChangeListener}
         />
 
         <button className="bg-indigo-800 hover:bg-indigo-700 text-white py-2 px-3 mt-5 rounded w-full">
--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,17 +1,20 @@
 import axios from 'axios'
-import 'dotenv/config'
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
+    baseURL: import.meta.env.VITE_BASE_URL,
     timeout: 1000,
-    headers: {'X-Custom-Header': 'foobar'}
+    headers: {
+        'X-Custom-Header': 'foobar',
+        'x-api-key': import.meta.env.VITE_API_KEY
+    }
 });
 
 function signup(username: string, email: string, password: string) {
     let req = api.post('/user/signup', {
         username, email, password
+    }).then(res => {
+        console.log(res.data)
     })
-    console.log(req)
 }
 
 export {
