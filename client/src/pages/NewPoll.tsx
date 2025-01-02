ad81776a5ae520fc41960684cdb840b6e794d80e
Omachonu Prosper <omachonucodes@gmail.com>
2025-01-02 19:04:30 +0100

    Set the title of the new poll page

--- client/src/pages/NewPoll.tsx
+++ client/src/pages/NewPoll.tsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+
 function NewPoll() {
+  useEffect(() => {
+    document.title = "New Poll - Pollarity";
+  }, []);
+
   return <div>Create a new poll here</div>;
 }
 
